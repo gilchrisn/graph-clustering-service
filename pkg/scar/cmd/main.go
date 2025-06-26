d291464768d9f9b41a4cc32bd653e324e1a0fd42
unknown <gilchrisnathaniel@gmail.com>
2025-06-26 18:56:47 +0700

    a bunch of debugging

--- pkg/scar/cmd/main.go
+++ pkg/scar/cmd/main.go
@@ -8,3 +8,5 @@ import (
 func main() {
 	scar.RunSCAR(os.Args[1:])
 }
+
+// To run: go build -o scar.exe && .\scar.exe "test_graph.txt" -pro="properties.txt" -path="path.txt" -k=6 -nk=1 -o="communities.txt" -louvain
